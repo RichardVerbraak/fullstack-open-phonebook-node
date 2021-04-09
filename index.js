6eefbd5ea58c3d6442aa0c4a0e684718b49f36c4
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2021-04-09 16:26:33 +0200

    Added routes for adding and deleting user

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 
-const persons = [
+let persons = [
 	{
 		id: 1,
 		name: 'Arto Hellas',
@@ -25,6 +25,8 @@ const persons = [
 	},
 ]
 
+app.use(express.json())
+
 app.get('/api/persons', (req, res) => {
 	res.send(persons)
 })
@@ -40,6 +42,48 @@ app.get('/info', (req, res) => {
         </div>
         `
 	)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+	const id = Number(req.params.id)
+
+	const found = persons.find((person) => {
+		return person.id === id
+	})
+
+	if (!found) {
+		res.status(404)
+		res.send('Not found')
+	} else {
+		res.status(200)
+		res.send(found)
+	}
+})
+
+app.post('/api/persons', (req, res) => {
+	const { name, number } = req.body
+
+	const id = Math.floor(Math.random() * 10000)
+
+	const person = {
+		id,
+		name,
+		number,
+	}
+
+	persons = [...persons, person]
+
+	res.send(persons)
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+	const id = Number(req.params.id)
+
+	const filtered = persons.filter((person) => {
+		return person.id !== id
+	})
+
+	res.send(filtered)
 })
 
 const PORT = 3001
